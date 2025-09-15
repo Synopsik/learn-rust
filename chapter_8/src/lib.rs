9106ab384011f27d3282919ec881c634def193eb
Synopsik <synopsikk@gmail.com>
2025-09-14 18:17:08 -0700

    Update: Applying DRY principles for formatting output

--- chapter_8/src/lib.rs
+++ chapter_8/src/lib.rs
@@ -1,17 +1,23 @@
 mod vectors;
+mod strings;
 
 pub fn run_all() {
+    let decorator_length = 5; 
     println!("Chapter 8: Common Collections");
-    separator_decorator(50);
     
-    println!("Vectors:");
-    vectors::run_examples();
-    separator_decorator(50);
-    
-    
+    run_example("Vectors", decorator_length, vectors::run_examples);
+    run_example("Strings", decorator_length, strings::run_examples);
 }
 
+fn run_example<F>(title: &str, length: usize, func: F) where F: FnOnce()
+{
+    separator_decorator(length);
+    println!("{}", title);
+    separator_decorator(length);
+    func();
+}
 fn separator_decorator(size: usize) 
 {
-    println!("{}", "~".repeat(size));
+    let chunk_size = 25;
+    println!("{}", "~".repeat(size * chunk_size));
 }
