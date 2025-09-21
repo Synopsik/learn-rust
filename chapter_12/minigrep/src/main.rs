ce8844be873e9d1d4911a9b444e89a1525940a9c
Synopsik <synopsikk@gmail.com>
2025-09-21 12:05:51 -0700

    Update: Removed the verbose println and updated others to be eprintln so that we can output to stderr and not stdout

--- chapter_12/minigrep/src/main.rs
+++ chapter_12/minigrep/src/main.rs
@@ -8,20 +8,15 @@ fn main() {
     
     // Create config
     let config = minigrep::Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
     
-    // Print args for user
-    println!("Search for word: {}\nIn the file: {}\n", 
-             config.query, config.file_path);
-    
     // Run program using config from args
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
-    
 }
 
 
