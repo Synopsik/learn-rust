852e70d0364271d14e4435d47710fbe23edd0428
Synopsik <synopsikk@gmail.com>
2025-10-16 21:46:04 -0700

    Update: Separated responsibilities into individual files

--- chapter_10/src/lib.rs
+++ chapter_10/src/lib.rs
@@ -1,140 +1,14 @@
+mod function_definitions;
+mod method_definitions;
+mod traits;
+
 pub fn run_all() {
-    function_definitions();
-    method_definitions();
-    implementing_traits();
-}
-
-// --------------------------------------------------------------------------------------------- //
-//                                  Function Definitions                                         //
-// --------------------------------------------------------------------------------------------- //
-
-fn function_definitions() {
-    let number_list = vec![34, 50, 25, 100, 65];
-    let char_list = vec!['y', 'm', 'a', 'q'];
-    
-    let result = largest(&number_list);
-    println!("The largest number is {result}");
-    
-    let result = largest(&char_list);
-    println!("The largest character is {result}");
-}
-
-fn largest<T: PartialOrd>(list: &[T]) -> &T {
-    let mut largest = &list[0];
-    
-    for item in list {
-        if item > &largest {
-            largest = item;
-        }
-    }
-    largest
+    function_definitions::run_function_definitions();
+    method_definitions::run_method_definitions();
+    traits::run_traits();
 }
 
 
-// --------------------------------------------------------------------------------------------- //
-//                                      Method Definitions                                       //
-// --------------------------------------------------------------------------------------------- //
 
-fn method_definitions() {
-    // Mixup struct using multiple Generic types
-    struct Point<X1, Y1> {
-        x: X1,
-        y: Y1,
-    }
-
-    // 
-    impl<X1, Y1> Point<X1, Y1> {
-        fn mixup<X2, Y2>(
-            self,
-            other: Point<X2, Y2>,
-        ) -> Point<X1, Y2> {
-            Point {
-                x: self.x,
-                y: other.y,
-            }
-        }
-    }
-    
-    let p1 = Point { x: 5, y: 10.4 };
-    let p2 = Point { x: "Hello", y: 'c'};
-    let p3 = p1.mixup(p2);
-    println!("p3.x = {}, p3.y = {}", p3.x, p3.y)
-}
-
-// --------------------------------------------------------------------------------------------- //
-//                                         Traits                                                //
-// --------------------------------------------------------------------------------------------- //
-pub trait Summary {
-    fn summarize(&self) -> String;
-}
-
-pub struct NewsArticle {
-    pub headline: String,
-    pub location: String,
-    pub author: String,
-    pub content: String,
-}
-
-impl Summary for NewsArticle {
-    fn summarize(&self) -> String {
-        format!(
-            "{}, by {} ({})",
-            self.headline,
-            self.author,
-            self.location
-        )
-    }
-}
-
-pub struct Tweet {
-    pub username: String,
-    pub content: String,
-    pub reply: bool,
-    pub retweet: bool,
-}
-
-impl Summary for Tweet {
-    fn summarize(&self) -> String {
-        format!("{}: {}", self.username, self.content)
-    }
-}
-
-fn implementing_traits() {
-    let tweet = Tweet {
-        username: String::from("horse_ebooks"),
-        content: String::from(
-            "of course, as you probably already know, people",
-        ),
-        reply: false,
-        retweet: false,
-    };
-    
-    let article = NewsArticle {
-        headline: String::from("Penguins win the Stanley Cup Championship!"),
-        location: String::from("Pittsburgh, PA, USA"),
-        author: String::from("Iceburgh"),
-        content: String::from(
-            "The Pittsburgh Penguins once again are the best hockey team in the NHL",
-        ),
-    };
-    
-    println!("1 new tweet: {}", tweet.summarize());
-    
-    println!("New article available! {}", article.summarize());
-    
-    notify(&tweet);    
-}
-
-pub fn notify(item: &impl Summary) {
-    println!("Breaking news! {}", item.summarize());
-}
-
-/* 
-Trait Bound Syntax without syntax sugar:
-
-pub fn notify<T: Summary>(item: &T) {
-    println!("Breaking news! {}", item.summarize());
-}
-*/
 
 
---
+++ chapter_10/src/traits.rs
@@ -0,0 +1,114 @@
+use std::fmt::{Debug, Display};
+use std::iter::Sum;
+
+pub fn run_traits() {
+    implementing_traits();
+}
+
+
+// --------------------------------------------------------------------------------------------- //
+//                                         Traits                                                //
+// --------------------------------------------------------------------------------------------- //
+pub trait Summary {
+    fn summarize(&self) -> String;
+}
+
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!(
+            "{}, by {} ({})",
+            self.headline,
+            self.author,
+            self.location
+        )
+    }
+}
+
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+}
+
+
+fn implementing_traits() {
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            "of course, as you probably already know, people",
+        ),
+        reply: false,
+        retweet: false,
+    };
+
+    let article = NewsArticle {
+        headline: String::from("Penguins win the Stanley Cup Championship!"),
+        location: String::from("Pittsburgh, PA, USA"),
+        author: String::from("Iceberg"),
+        content: String::from(
+            "The Pittsburgh Penguins once again are the best hockey team in the NHL",
+        ),
+    };
+
+    println!("1 new tweet: {}", tweet.summarize()); // Custom summarize method for Tweets
+    println!("New article available! {}", article.summarize()); // And Articles
+
+    notify(&tweet); // Can only call this on objects that implement the Summarize trait
+    notify_expanded(&tweet, &tweet);
+}
+
+
+pub fn notify(item: &impl Summary) {
+// Traits on function definitions.
+// Only arguments that implement the Summary trait can use this function
+    println!("Breaking news! {}", item.summarize());
+}
+
+
+// Trait Bound Syntax without syntax sugar:
+pub fn notify_expanded<T: Summary>(item1: &T, item2: &T) {
+    // This can be useful if you need to force multiple args to have the same trait
+    println!("Breaking news! {}", item1.summarize());
+    println!("Afterwards, {}", item2.summarize());
+}
+
+
+// Where Clauses
+// If you have too many trait bounds, 
+// a `where` clause can be used to declutter the function signature
+fn some_function<T, U>(t: &T, u: &U) -> i32
+where
+    T: Display + Clone,
+    U: Clone + Debug,
+{
+    42   
+}
+
+
+// You can also create a function that returns a value that must match a trait
+fn returns_summarizable() -> impl Summary {
+    Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    }
+}
