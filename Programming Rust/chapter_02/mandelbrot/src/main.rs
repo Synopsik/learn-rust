71ee006ebde6a226d25d1e07bd934e1989895265
Synopsik <synopsikk@gmail.com>
2025-10-31 15:17:26 -0700

    Update: Minor cleanup and updating main README

--- Programming Rust/chapter_02/mandelbrot/src/main.rs
+++ Programming Rust/chapter_02/mandelbrot/src/main.rs
@@ -21,17 +21,19 @@ fn main() {
         std::process::exit(1);
     }
 
-    let bounds = parse_pair(&args[2], 'x').expect("error parsing image dimensions");
+    let bounds =
+        parse_pair(&args[2], 'x').expect("error parsing image dimensions");
 
-    let upper_left = parse_complex(&args[3]).expect("Error parsing upper left corner point");
+    let upper_left =
+        parse_complex(&args[3]).expect("Error parsing upper left corner point");
 
-    let lower_right = parse_complex(&args[4]).expect("error parsing lower right corner point");
+    let lower_right =
+        parse_complex(&args[4]).expect("error parsing lower right corner point");
 
     let mut pixels = vec![0; bounds.0 * bounds.1];
 
     // render(&mut pixels, bounds, upper_left, lower_right);
     render_parallel(&mut pixels, bounds, upper_left, lower_right);
-    
 
     write_image(&args[1], &pixels, bounds).expect("error writing PNG file")
 }
--- Programming Rust/chapter_02/mandelbrot/src/parallelism.rs
+++ Programming Rust/chapter_02/mandelbrot/src/parallelism.rs
@@ -2,6 +2,7 @@ use num::Complex;
 use crossbeam;
 use crate::image::{pixel_to_point, render};
 
+
 pub fn render_parallel(pixels: &mut [u8],
                        bounds: (usize, usize),
                        upper_left: Complex<f64>,
@@ -17,8 +18,10 @@ pub fn render_parallel(pixels: &mut [u8],
             let top = rows_per_band * i;
             let height = band.len() / bounds.0;
             let band_bounds = (bounds.0, height);
-            let band_upper_left = pixel_to_point(bounds, (0, top), upper_left, lower_right);
-            let band_lower_right = pixel_to_point(bounds, (bounds.0, top + height), upper_left, lower_right);
+            let band_upper_left = 
+                pixel_to_point(bounds, (0, top), upper_left, lower_right);
+            let band_lower_right = 
+                pixel_to_point(bounds, (bounds.0, top + height), upper_left, lower_right);
             
             spawner.spawn(move |_| {
                 render(band, band_bounds, band_upper_left, band_lower_right);
